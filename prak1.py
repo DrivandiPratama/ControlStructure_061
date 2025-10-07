3dd9d82400552bee45e6db973b646d7ed0f41542
DrivandiPratama <dripandipratama@gmail.com>
2025-10-07 15:21:41 +0700

    Membuat program nilai siswa

---
+++ prak1.py
@@ -0,0 +1,12 @@
+percentage = float(input("Masukkan persentase nilai siswa: "))
+
+if percentage >= 90:    
+     print("Excelent performance")
+elif percentage >= 80:
+     print("Very good performance")
+elif percentage >= 70:
+    print("Good performance")
+elif percentage >= 60:
+    print("Avarage performance")
+else:
+    print("Needs Improvement")
